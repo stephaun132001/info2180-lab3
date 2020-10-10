6a55cc7834053e2e1a1f51b516fd04b647e4406c
steph jarrett <s.jatter12345@gmail.com>
2020-10-09 20:57:37 -0500

    starting completely over

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,61 +1,101 @@
-
 document.addEventListener("DOMContentLoaded", function(){
-var divs = document.getElementById("board");
-for (var i=0; i<9; i++){
-    divs.getElementsByTagName('div')[i].classList.add("square");
-    
-}
-
-const squares = (Array.from(document.querySelectorAll('#board div')));
-
+let squares = document.querySelectorAll('#board div')
+let circleTurn 
 const X_class = 'X';
 const O_class = 'O';
 
-const gridCells = document.querySelectorAll('[squares]');
-
-squares.forEach(gridCells =>{
-    gridCells.addEventListener('mouseover', handlemouseover)
+for (let i = 0; i < squares.length; i++){
+    squares[i].classList.add('square', "X");
+    squares[i].addEventListener('click', function(){
+        console.log('Square ' + i + 'clicked')
+        const currentClass = circleTurn ? O_class: X_class
+        circleTurn = !circleTurn
+        squares[i].innerHTML = currentClass;
+        clicked();
+        //console.log(squares);
+        
+        
+    })
+}
 })
-    squares.forEach(gridCells =>{
-        gridCells.addEventListener('mouseout', handlemouseout)
 
-})
 
-squares.forEach(gridCells =>{
-    gridCells.addEventListener('click', handleClick,{once: true})
-})
-let circleTurn 
- function handleClick(e){
-     const gridCells = e.target
-     const currentClass = circleTurn ? O_class: X_class
-     
-     placeMark(gridCells, currentClass)
-     
-     
-     swapTurns()
- }
 
- function placeMark(gridCells, currentClass){
-     gridCells.innerHTML = currentClass
-     
-     
-}
-function swapTurns(){
-    circleTurn = !circleTurn
-}
-function handlemouseover(e){
-    const gridCells = e.target
-    gridCells.classList.add("hover");
-}
 
-function handlemouseout(e){
-    const gridCells = e.target
-    this.setAttribute("class", "square");
-}
-function checkwin(){
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function(){
+// var divs = document.getElementById("board");
+// for (var i=0; i<9; i++){
+//     divs.getElementsByTagName('div')[i].classList.add("square");
     
-}
-})
+// }
+
+// const squares = (Array.from(document.querySelectorAll('#board div')));
+
+// var boardstatus = new Array(0);   
+//  winner = 'winner';
+// const X_class = 'X';
+// const O_class = 'O';
+
+
+
+// const gridCells = document.querySelectorAll('[squares]');
+
+// squares.forEach(gridCells =>{
+//     gridCells.addEventListener('mouseover', handlemouseover)
+// })
+//     squares.forEach(gridCells =>{
+//         gridCells.addEventListener('mouseout', handlemouseout)
+// })
+
+// squares.forEach(gridCells =>{
+//     gridCells.addEventListener('click', handleClick,{once: true})
+// })
+// let circleTurn 
+//  function handleClick(e){
+//      const gridCells = e.target
+//      const currentClass = circleTurn ? O_class: X_class
+//      placeMark(gridCells, currentClass);
+//      checkwin();
+//      var board = Array.prototype.slice.call(gridCells.parentElement.children).indexOf(gridCells)
+//      console.log(board);
+//      boardstatus= [currentClass];
+//      console.log(boardstatus);
+    
+//     swapTurns();
+    
+    
+//  }
+
+//  function placeMark(gridCells, currentClass){
+//      gridCells.innerHTML = currentClass
+     
+     
+// }
+// function swapTurns(){
+//     circleTurn = !circleTurn
+// }
+// function handlemouseover(e){
+//     const gridCells = e.target
+//     gridCells.classList.add("hover");
+// }
+
+// function handlemouseout(e){
+//     const gridCells = e.target
+//     this.setAttribute("class", "square");
+// }
+ 
+
+// function checkwin(boardstatus){
+//     if (boardstatus[0].innerHTML === 'X' && boardstatus[0].innerHTML === boardstatus[1].innerHTML && boardstatus[0].innerHTML === boardstatus[2].innerHTML){
+//         document.getElementById('status').className = "you-won"
+//         document.getElementById('status').innerHTML =   "Congratulations! "+ boardstatus[0] + " is the Winner!"
+//      }
+//     }
+// })
 
 
 
