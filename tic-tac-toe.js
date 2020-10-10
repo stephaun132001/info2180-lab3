461617c33deaec8d3c7a32718a519c63abb344b1
steph jarrett <s.jatter12345@gmail.com>
2020-10-09 23:21:42 -0500

    restart and disallowed cheating implemented

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -65,7 +65,10 @@ for (var x = 0; x < squares.length; x++){
                 document.getElementById("status").className=("you-won");
             }
         }
-        
+        var restartbtn = document.querySelector("button");
+	    restartbtn.addEventListener("click",function(){
+		window.location.reload();		
+	})
 })
 
 
@@ -75,73 +78,3 @@ for (var x = 0; x < squares.length; x++){
 
 
 
-// document.addEventListener("DOMContentLoaded", function(){
-// var divs = document.getElementById("board");
-// for (var i=0; i<9; i++){
-//     divs.getElementsByTagName('div')[i].classList.add("square");
-    
-// }
-
-// const squares = (Array.from(document.querySelectorAll('#board div')));
-
-// var boardstatus = new Array(0);   
-//  winner = 'winner';
-// const X_class = 'X';
-// const O_class = 'O';
-
-
-
-// const gridCells = document.querySelectorAll('[squares]');
-
-// squares.forEach(gridCells =>{
-//     gridCells.addEventListener('mouseover', handlemouseover)
-// })
-//     squares.forEach(gridCells =>{
-//         gridCells.addEventListener('mouseout', handlemouseout)
-// })
-
-// squares.forEach(gridCells =>{
-//     gridCells.addEventListener('click', handleClick,{once: true})
-// })
-// let circleTurn 
-//  function handleClick(e){
-//      const gridCells = e.target
-//      const currentClass = circleTurn ? O_class: X_class
-//      placeMark(gridCells, currentClass);
-//      checkwin();
-//      var board = Array.prototype.slice.call(gridCells.parentElement.children).indexOf(gridCells)
-//      console.log(board);
-//      boardstatus= [currentClass];
-//      console.log(boardstatus);
-    
-//     swapTurns();
-    
-    
-//  }
-
-//  function placeMark(gridCells, currentClass){
-//      gridCells.innerHTML = currentClass
-     
-     
-// }
-// function swapTurns(){
-//     circleTurn = !circleTurn
-// }
-// function handlemouseover(e){
-//     const gridCells = e.target
-//     gridCells.classList.add("hover");
-// }
-
-// function handlemouseout(e){
-//     const gridCells = e.target
-//     this.setAttribute("class", "square");
-// }
- 
-
-// function checkwin(boardstatus){
-//     if (boardstatus[0].innerHTML === 'X' && boardstatus[0].innerHTML === boardstatus[1].innerHTML && boardstatus[0].innerHTML === boardstatus[2].innerHTML){
-//         document.getElementById('status').className = "you-won"
-//         document.getElementById('status').innerHTML =   "Congratulations! "+ boardstatus[0] + " is the Winner!"
-//      }
-//     }
-// })
